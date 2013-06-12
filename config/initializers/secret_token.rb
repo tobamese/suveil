692a45d41ed220ec93c34f628fab0f94857c2114
tobamese <me@lizlara.com>
2013-06-11 22:40:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suveil::Application.config.secret_token = '88a012e294d90e3484b74d852f698d6ab8e7a9317b8f85a6d8fdddadcd87a5de6a3ace4f78a6723dcb9eb9828187107db09ccc9ca9d529c1128e0ee318c060e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Suveil::Application.config.session_store :cookie_store, :key => '_suveil_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Suveil::Application.config.session_store :active_record_store
